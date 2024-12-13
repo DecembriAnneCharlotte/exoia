2112841e245ba3fa92e5654ce611f69c444298f1
unknown <135713507+DecembriAnneCharlotte@users.noreply.github.com>
2024-12-13 13:31:49 +0100

    fix url locale

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,7 +5,9 @@ function App() {
 	const [message, setMessage] = useState("");
 
 	useEffect(() => {
-		fetch("http://localhost:8000/")
+		// fetch("http://localhost:8000/")
+		fetch("http://34.155.24.177:8000")
+
 			.then((response) => response.json())
 			.then((data) => setMessage(data.message));
 	}, []);
